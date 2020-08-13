6f6a77a82b75d446feb6a9fb7fc8559347a052e6
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-13 02:29:10 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -11,4 +11,8 @@
 def select_winner(passengers)
   # add the code snippet here!
   passengers.each do |suite, name|
+    if suite ==suite_a && name.start_with?("A")
+      puts name
+    end
+  end
 end

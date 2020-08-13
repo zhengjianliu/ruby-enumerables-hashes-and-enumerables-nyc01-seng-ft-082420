9dcf90323fc5c66a35f112d19abde51533c07130
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-13 02:30:19 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,6 +4,11 @@
 #   "Amanda" => 27
 # }
 
+#Happy Birthday Timmy! You are now 9 years old!
+
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids.each do |name,age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  end
 end
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,6 +6,15 @@
 # suite_e: "Crumpet the Elf"
 # }
 
+#Amanda Presley
+
 def select_winner(passengers)
   # add the code snippet here!
+  winner =""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
+  end
+  winner
 end
